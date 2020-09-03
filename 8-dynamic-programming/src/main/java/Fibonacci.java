54121b43c5076e0a8e7ea30858873d4737312a36
hacktuarial <tim.sweetser@findmine.com>
2020-09-03 17:02:18 -0400

    memoized takes 22 ms

--- 8-dynamic-programming/src/main/java/Fibonacci.java
+++ 8-dynamic-programming/src/main/java/Fibonacci.java
@@ -1,10 +1,20 @@
 public class Fibonacci {
-    public int number(int n) {
+    public int calculate_recursive(int n) {
         if (n < 2) {
             return n;
         }
         else {
-            return number(n-1) + number(n-2);
+            return calculate_recursive(n-1) + calculate_recursive(n-2);
         }
+    }
+
+    public int calculate_memoized(int n) {
+        int[] results = new int[n+1];
+        results[0] = 0;
+        results[1] = 1;
+        for (int i=2; i <= n; i++) {
+            results[i] = results[i-1] + results[i-2];
+        }
+        return results[n];
     }
 }
