0c3ccec475a8480d84875559151bb3ba3fe9e8d2
hacktuarial <tim.sweetser@findmine.com>
2020-09-03 16:44:43 -0400

    tests fail, intentionally

--- 8-dynamic-programming/src/test/java/FibonacciTest.java
+++ 8-dynamic-programming/src/test/java/FibonacciTest.java
@@ -1,13 +1,15 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class FibonacciTest {
 
-    Fibonacci Fib = new Fibonacci();
+
 
     @Test
-    void myFirstTest() {
-        assertEquals(Fib.number(1), 1);
+    void TestOne() {
+        Fibonacci fib = new Fibonacci();
+        assertEquals(fib.number(1), 100);
     }
+
 }
